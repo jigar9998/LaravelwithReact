c023ab8970e8de4a4efe8fb386e3fd09b49b0b00
Jigar S Panchal <jigars.panchal@sparsh1.com>
2020-02-03 14:15:59 +0530

    update on 3-2-2020

---
+++ Demoproject/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    protected function register(Request $request )
+    {
+        $this-> validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $data =  User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => bcrypt($request['password']),
+        ]);
+
+        return response()->json('Successfully added');
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+
+
+    protected function login(Request $request){
+        $user = Auth::attempt(['email' => $request->email, 'password' => $request->password]);
+        if ($user) {
+            $user_data = User::all()->where('email', $request->email );
+            // Authentication passed...
+            return response()->json(Auth::user());
+        }else{
+            
+        }
+    }
+    protected function logout(Request $request) {
+        Auth::logout();
+        
+        return response()->json('logout Successfully');
+    }
+}
---
+++ Demoproject/resources/assets/js/components/Dashboard.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+//import {  Router, Route , Link , Switch } from 'react-router-dom';
+import  history from './history';
+
+
+class Dashboard extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            Name : this.props.location.data.name
+    }
+        this.handleclick = this.handleclick.bind(this);
+    }
+
+    handleclick(){
+        axios.post('http://127.0.0.1:8000/logout', {
+        })
+        .then(response=> {
+            history.push('/login');
+        })
+        .catch(error=> {
+            console.log(error);
+        })
+    }
+
+    render(){
+        let data = this.state.Name
+        console.log(data);
+        const mystyle = {  
+            float: 'left',
+            paddingLeft: '109px',
+            color: '#274f69',
+            fontSize: '50px',
+            fontWeight: '600',
+            fontFamily: 'Roboto,sans-serif',
+            paddingTop: '13px'
+          };  
+        return(
+            <div className="row">
+                <div className="col-md-12 wrapper ">
+                    <div className="user_details">
+                        <span style={mystyle} > Dashboard </span>
+                        <span>Current User :- </span>  {this.state.Name} <br />
+                        <button type="submit" className="btn btn-primary" onClick={this.handleclick} > LogOut </button> 
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Dashboard;
